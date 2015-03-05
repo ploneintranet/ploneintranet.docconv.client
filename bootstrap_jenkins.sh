3fb32c1316a74c84b19aa6fbc5ae96cae02159eb
Manuel Reinhardt <reinhardt@syslab.com>
2015-03-05 09:47:08 +0000

    install ruby gem bundle

--- bootstrap_jenkins.sh
+++ bootstrap_jenkins.sh
@@ -2,5 +2,6 @@ virtualenv --no-setuptools .
 mkdir -p buildout-cache/downloads
 ./bin/python bootstrap.py -v 1.6.3
 ./bin/buildout -N -t 3 -c jenkins.cfg
+bundle install
 ./bin/develop up -f
 # Xvfb :99 -a &
