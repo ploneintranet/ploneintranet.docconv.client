c367b3a788ad41275752ff788ead818b9c8c1623
Alexander Pilz <pilz@syslab.com>
2015-03-05 10:33:58 +0100

    state correct config instead of overriding buildout

--- bootstrap_jenkins.sh
+++ bootstrap_jenkins.sh
@@ -1,7 +1,6 @@
 virtualenv --no-setuptools .
 mkdir -p buildout-cache/downloads
-$(which echo) -e "[buildout]\nextends = buildout.d/travis.cfg" > buildout.cfg
 ./bin/python bootstrap.py -v 1.6.3
-./bin/buildout
+./bin/buildout -N -t 3 -c travis.cfg
 ./bin/develop up -f
 # Xvfb :99 -a &
