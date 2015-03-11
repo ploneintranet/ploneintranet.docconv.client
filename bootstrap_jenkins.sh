65382995c4124cc39c11b81d9425192d659f9ecb
Manuel Reinhardt <reinhardt@syslab.com>
2015-03-11 20:44:56 +0100

    help jenkins find docsplit

--- bootstrap_jenkins.sh
+++ bootstrap_jenkins.sh
@@ -2,6 +2,6 @@ virtualenv --no-setuptools .
 mkdir -p buildout-cache/downloads
 ./bin/python bootstrap.py -v 1.6.3
 ./bin/buildout -N -t 3 -c jenkins.cfg
-bundle install --path vendor/bundle
+bundle install --path vendor/bundle --binstubs
 ./bin/develop up -f
 # Xvfb :99 -a &
